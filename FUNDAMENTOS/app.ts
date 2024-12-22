a40c8a276ef88d18d4679ca140c532217eaeee7f
Cybervtx <149208626+Cybervtx@users.noreply.github.com>
2024-12-22 20:00:53 +0000

    code

--- FUNDAMENTOS/app.ts
+++ FUNDAMENTOS/app.ts
@@ -5,8 +5,9 @@ import { PeopleAccount } from "./class/PeopleAccount"
 const peopleAccount: PeopleAccount = new PeopleAccount(1, "Diogo", 1)
 const companyAccount: CompanyAccount = new CompanyAccount("Dio", 20)
 
-peopleAccount.deposit()
-companyAccount.deposit()
+console.log(peopleAccount)
+peopleAccount.setName('Diogo Caldas')
+console.log(peopleAccount.getName())
 
 
 // console.log(peopleAccount)
